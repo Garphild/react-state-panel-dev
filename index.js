96892aadead72506c77fbce5fecde0f30b6abcec
Serhii Kondrashov <garphild28@gmail.com>
2020-03-22 16:11:27 +0200

    upd

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-if (process.env.NODE_ENV === 'development') {
-  // eslint-disable-next-line global-require
+// if (process.env.NODE_ENV === 'development') {
+//   // eslint-disable-next-line global-require
   module.exports = require('./build/StateObserver').default;
-} else {
-  module.exports = process.env.NODE_ENV;
-}
+// } else {
+//   module.exports = process.env.NODE_ENV;
+// }
